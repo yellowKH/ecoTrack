d0ce0bcc5eb0fc0903f3a73a33adaff14997d9a9
kigari94 <36a8d9lf21x>
2021-05-17 21:25:07 +0200

    implementing flatList as ArticleList component, implementing articleInfo as ArticleItem component, adding first data model

---
+++ components/ArticleList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, FlatList } from 'react-native';
+
+// Dummy data import
+import ArticleItem from './ArticleItem'
+import { ARTICLEITEMS } from '../data/dummy-data'
+
+export default ArticleList = props => {
+
+    const displayedArticle = ARTICLEITEMS;
+
+    const renderItemHandler = itemData => {
+        return <ArticleItem
+            title={itemData.item.title}
+        />
+    }
+
+    return (
+        <FlatList
+            data={displayedArticle}
+            style={{ width: '100%' }}
+            renderItem={renderItemHandler}
+            contentContainerStyle={{ alignItems: 'stretch', padding: 20 }}
+            keyExtractor={(item, index) => item.id}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    contentContainerStyle: {
+        width: '100%',
+        alignItems: 'center'
+    },
+});
---
+++ models/articleItem.js
@@ -0,0 +1,10 @@
+class ArticleItem {
+    constructor(id, title, isFav) {
+      this.id = id;
+      this.title = title;
+      this.isFav = isFav;
+    }
+  }
+  
+  export default ArticleItem;
+  
