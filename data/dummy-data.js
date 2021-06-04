5c10eb58360563003ebdc8aece905e8e9e53e1af
Nhilpferd <lannhile@hotmail.com>
2021-06-04 17:16:28 +0200

    Added new dummy data

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -10,6 +10,88 @@ export const ARTICLEITEMS = [
     "https://toppng.com/uploads/preview/coca-cola-bottle-11528338683o0snrfbxt5.png",
     2
   ),
+  
+  new ArticleItem(
+    "837423",
+    "Melitta Filtercoffee 500g",
+    "Perfect for working at midnight",
+    "https://www.melitta.de/media/image/ca/9f/0f/Kaffee-Melitta-Filterkaffee-Auslese-5680498_600x600.png",
+    3
+  ),
+
+   new ArticleItem(
+    "238764",
+    "Peanuts 50g",
+    "The best good looking peanuts",
+    "https://pngimg.com/uploads/peanut/peanut_PNG41.png",
+    10
+  ),  
+
+  new ArticleItem(
+    "244838",
+    "Potatoes 1kg",
+    "Sunny potatoes",
+    "https://image.similarpng.com/very-thumbnail/2020/06/Raw-potatoes-in-burlap-bag-on-transparent-background-PNG.png",
+    4
+  ),
+
+  new ArticleItem(
+    "732832",
+    "Chicken 1.5kg",
+    "Chicken is definitely better than Beef",
+    "https://e7.pngegg.com/pngimages/800/455/png-clipart-fresh-whole-chicken-whole-chicken-chicken.png",
+    5
+  ),
+
+  new ArticleItem(
+    "342358",
+    "Rice 1kg",
+    "Buy Rice, then you are nice",
+    "https://e7.pngegg.com/pngimages/597/57/png-clipart-rice-meter-rice.png",
+    1
+  ),
+  
+  new ArticleItem(
+    "122934",
+    "Eggs 6pcs",
+    "Different looking cute Eggs",
+    "https://e7.pngegg.com/pngimages/979/262/png-clipart-eggs-eggs-thumbnail.png",
+    8
+  ),
+
+  new ArticleItem(
+    "823726",
+    "Carrots in bundle",
+    "Carrots good for eyes",
+    "https://w7.pngwing.com/pngs/833/863/png-transparent-juice-baby-carrot-vegetable-rabbit-carrot-plant-natural-foods-soup-food.png",
+    3
+  ),
+
+  new ArticleItem(
+    "732652",
+    "Milk",
+    "Best milk you will ever drink in your life",
+    "https://www.konsum-leipzig.de/media/image/fe/9a/43/DV019_4008452027602_PERewUfzmtDRKNrk_600x600.png",
+    9
+  ),
+    
+  new ArticleItem(
+    "584523",
+    "Onions 1kg",
+    "Fresh and good onions",
+    "http://assets.stickpng.com/images/580b57fcd9996e24bc43c21d.png",
+    10
+  ),
+
+  new ArticleItem(
+    "237394",
+    "Lemons 500g",
+    "If life give you lemons, make lemonade",
+    "https://e7.pngegg.com/pngimages/577/255/png-clipart-lemon-lemon.png",
+    5
+  ),
+
+
 ];
 
 export const BOUGHTITEMS = [
