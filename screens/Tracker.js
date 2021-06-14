53c6143b889cf3c8892f38f0f1262db611665f3c
KHome <kevin.hoppe@lemonswan.com>
2021-06-14 20:08:27 +0200

    hand period state over to chart component

--- screens/Tracker.js
+++ screens/Tracker.js
@@ -1,15 +1,22 @@
 import React, { useContext, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import ArticleList from "../components/ArticleList";
 import Chart from "../components/Chart";
 import { ArticleContext } from "../data/ArticleContext";
 
 export default Tracker = (props) => {
   const [articleData] = useContext(ArticleContext);
+  const [period, setPeriod] = useState("total");
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Chart boughtArticles={articleData.boughtArticles} />
+      <View style={{ flexDirection: "row", justifyContent: "center", marginTop: 20 }}>
+        <Button title="day" onPress={() => setPeriod("day")} />
+        <Button title="month" onPress={() => setPeriod("month")} />
+        <Button title="year" onPress={() => setPeriod("year")} />
+        <Button title="total" onPress={() => setPeriod("total")} />
+      </View>
+      <Chart boughtArticles={articleData.boughtArticles} period={period} />
       <Text>Your average CO2 Score: {articleData.average}</Text>
       <ArticleList target={"Article Info"} articles={articleData.boughtArticles} />
     </View>
