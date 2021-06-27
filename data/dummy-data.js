a4e21a1958a4b2d953a323aba8231b34d2215a9e
KHome <kevin.hoppe@lemonswan.com>
2021-06-27 19:17:10 +0200

    code cleaning and working png images

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,15 +1,7 @@
 import ArticleItem from "../models/articleItem";
-import BoughtItem from "../models/boughtItem";
-import FavorizedItem from "../models/favorizedItem";
 
 export const ARTICLEITEMS = [
-  new ArticleItem(
-    "123456",
-    "Coca Cola 1,5L",
-    "Also perfectly usable as bleach",
-    "https://toppng.com/uploads/preview/coca-cola-bottle-11528338683o0snrfbxt5.png",
-    2
-  ),
+  new ArticleItem("123456", "Coca Cola 1,5L", "Also perfectly usable as bleach", "https://img.rewe-static.de/6729754/1244210_digital-image.png", 2),
 
   new ArticleItem(
     "837423",
@@ -21,31 +13,37 @@ export const ARTICLEITEMS = [
 
   new ArticleItem("238764", "Peanuts 50g", "The best good looking peanuts", "https://pngimg.com/uploads/peanut/peanut_PNG41.png", 10),
 
-  new ArticleItem(
-    "244838",
-    "Potatoes 1kg",
-    "Sunny potatoes",
-    "https://image.similarpng.com/very-thumbnail/2020/06/Raw-potatoes-in-burlap-bag-on-transparent-background-PNG.png",
-    4
-  ),
+  new ArticleItem("244838", "Potatoes 1kg", "Sunny potatoes", "https://cdn.shopify.com/s/files/1/2297/2851/products/Potatoesper0.5kg_800x800.png", 4),
 
   new ArticleItem(
     "732832",
     "Chicken 1.5kg",
     "Chicken is definitely better than Beef",
-    "https://e7.pngegg.com/pngimages/800/455/png-clipart-fresh-whole-chicken-whole-chicken-chicken.png",
+    "https://www.pngarts.com/files/3/Chicken-Meat-Transparent-Images.png",
     5
   ),
 
-  new ArticleItem("342358", "Rice 1kg", "Buy Rice, then you are nice", "https://e7.pngegg.com/pngimages/597/57/png-clipart-rice-meter-rice.png", 1),
+  new ArticleItem(
+    "342358",
+    "Wheat 1kg",
+    "Buy Wheat, then you are sweet",
+    "https://purepng.com/public/uploads/large/purepng.com-flourflourgrainscerealbread-1411527418608hl1sb.png",
+    1
+  ),
 
-  new ArticleItem("122934", "Eggs 6pcs", "Different looking cute Eggs", "https://e7.pngegg.com/pngimages/979/262/png-clipart-eggs-eggs-thumbnail.png", 8),
+  new ArticleItem(
+    "122934",
+    "Eggs 6pcs",
+    "Different looking cute Eggs",
+    "https://purepng.com/public/uploads/large/purepng.com-eggseggseggshellegg-whiteegg-yolk-1411527413521anwtz.png",
+    8
+  ),
 
   new ArticleItem(
     "823726",
     "Carrots in bundle",
     "Carrots good for eyes",
-    "https://w7.pngwing.com/pngs/833/863/png-transparent-juice-baby-carrot-vegetable-rabbit-carrot-plant-natural-foods-soup-food.png",
+    "https://cdn.shopify.com/s/files/1/0441/6254/7869/products/55-Carrots-per-kg-removebg-preview_500x.png?v=1595660849",
     3
   ),
 
@@ -57,11 +55,17 @@ export const ARTICLEITEMS = [
     9
   ),
 
-  new ArticleItem("584523", "Onions 1kg", "Fresh and good onions", "http://assets.stickpng.com/images/580b57fcd9996e24bc43c21d.png", 10),
+  new ArticleItem(
+    "584523",
+    "Onions 1kg",
+    "Fresh and good onions",
+    "https://www.coffeecompany.co.za/pub/media/catalog/product/cache/1583e2c6bd2ebbdd02756c1fcdc56280/o/n/onions.png",
+    10
+  ),
 
-  new ArticleItem("237394", "Lemons 500g", "If life give you lemons, make lemonade", "https://e7.pngegg.com/pngimages/577/255/png-clipart-lemon-lemon.png", 5),
+  new ArticleItem("237394", "Lemons 500g", "If life give you lemons, make lemonade", "https://www.freeiconspng.com/thumbs/lemon-png/lemon-png-3.png", 5),
 
-  new ArticleItem("654321", "Tomaten", "Super delicious tomatos", "https://www.kochschule.de/sites/default/files/images/kochwissen/461/tomate.jpg", 7),
+  new ArticleItem("654321", "Tomaten", "Super delicious tomatos", "https://assets.stickpng.com/images/580b57fcd9996e24bc43c23b.png", 7),
 ];
 
 export const BOUGHTITEMS = [];
